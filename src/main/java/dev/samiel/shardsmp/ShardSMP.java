7814e616fef8d7a30a73beb2219f92a11ed8a23f
SamielXD2 <samiellxd@gmail.com>
2026-02-21 22:47:07 +0800

    Update ShardSMP.java

--- src/main/java/dev/samiel/shardsmp/ShardSMP.java
+++ src/main/java/dev/samiel/shardsmp/ShardSMP.java
@@ -1,5 +1,6 @@
 package dev.samiel.shardsmp;
 
+import dev.samiel.shardsmp.command.ShardCommand;
 import dev.samiel.shardsmp.listener.ShardListener;
 import dev.samiel.shardsmp.manager.ShardManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,6 +13,9 @@ public class ShardSMP extends JavaPlugin {
     public void onEnable() {
         shardManager = new ShardManager(this);
         getServer().getPluginManager().registerEvents(new ShardListener(this, shardManager), this);
+        ShardCommand shardCommand = new ShardCommand(this, shardManager);
+        getCommand("shards").setExecutor(shardCommand);
+        getCommand("spl").setExecutor(shardCommand);
         getServer().getScheduler().runTaskTimer(this, () -> {
             for (org.bukkit.entity.Player player : getServer().getOnlinePlayers()) {
                 shardManager.applyPassiveEffects(player);
