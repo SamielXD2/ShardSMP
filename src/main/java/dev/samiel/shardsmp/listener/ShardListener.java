ec28775fd584f02b11d112619b4e76fd1fc2e213
SamielXD2 <samiellxd@gmail.com>
2026-02-22 06:55:18 +0800

    Update ShardListener.java

--- src/main/java/dev/samiel/shardsmp/listener/ShardListener.java
+++ src/main/java/dev/samiel/shardsmp/listener/ShardListener.java
@@ -9,6 +9,7 @@ import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.EquipmentSlot;
 
 import java.util.HashMap;
@@ -30,6 +31,11 @@ public class ShardListener implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         shardManager.initPlayer(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        shardManager.savePlayer(event.getPlayer());
     }
 
     @EventHandler
