770d548c8ae4fb501b29cef11d50b7e5c37a5a3b
SamielXD2 <samiellxd@gmail.com>
2026-02-21 22:04:54 +0800

    Create src/main/java/dev/samiel/shardsmp/shardsmp.java

---
+++ src/main/java/dev/samiel/shardsmp/shardsmp.java
@@ -0,0 +1,26 @@
+package dev.samiel.shardsmp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShardSMP extends JavaPlugin {
+
+    private ShardManager shardManager;
+
+    @Override
+    public void onEnable() {
+        shardManager = new ShardManager(this);
+        getServer().getPluginManager().registerEvents(new ShardListener(this, shardManager), this);
+        getServer().getScheduler().runTaskTimer(this, () -> {
+            for (org.bukkit.entity.Player player : getServer().getOnlinePlayers()) {
+                shardManager.applyPassiveEffects(player);
+            }
+        }, 20L, 20L);
+    }
+
+    @Override
+    public void onDisable() {}
+
+    public ShardManager getShardManager() {
+        return shardManager;
+    }
+}
