9493e7e45e9d16ef034e02bb91846b77a23fb215
SamielXD2 <samiellxd@gmail.com>
2026-02-22 08:51:00 +0800

    Update ShardSMP.java

--- src/main/java/dev/samiel/shardsmp/ShardSMP.java
+++ src/main/java/dev/samiel/shardsmp/ShardSMP.java
@@ -4,6 +4,7 @@ import dev.samiel.shardsmp.command.ShardCommand;
 import dev.samiel.shardsmp.listener.ShardListener;
 import dev.samiel.shardsmp.manager.ShardManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.PluginCommand;
 
 public class ShardSMP extends JavaPlugin {
 
@@ -14,9 +15,20 @@ public class ShardSMP extends JavaPlugin {
         shardManager = new ShardManager(this);
         getServer().getPluginManager().registerEvents(new ShardListener(this, shardManager), this);
         ShardCommand shardCommand = new ShardCommand(this, shardManager);
-        getCommand("shardx").setExecutor(shardCommand);
-        getCommand("spl").setExecutor(shardCommand);
-        getCommand("shardadmin").setExecutor(shardCommand);
+
+        PluginCommand shardx = getCommand("shardx");
+        PluginCommand spl = getCommand("spl");
+        PluginCommand shardadmin = getCommand("shardadmin");
+
+        if (shardx != null) shardx.setExecutor(shardCommand);
+        else getLogger().severe("Command 'shardx' not found in plugin.yml!");
+
+        if (spl != null) spl.setExecutor(shardCommand);
+        else getLogger().severe("Command 'spl' not found in plugin.yml!");
+
+        if (shardadmin != null) shardadmin.setExecutor(shardCommand);
+        else getLogger().severe("Command 'shardadmin' not found in plugin.yml!");
+
         getServer().getScheduler().runTaskTimer(this, () -> {
             for (org.bukkit.entity.Player player : getServer().getOnlinePlayers()) {
                 shardManager.applyPassiveEffects(player);
